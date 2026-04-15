e71c627743b16bcb3700f2ec3a2c0c71f147c76d
Anvar Obidov <varan193@mail.ru>
2026-04-16 02:39:22 +0300

    Урок 1

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+// Создание переменных
+let title;
+let screens;
+let screenprice;
+let rollback;
+let fullprice;
+let adaptive;
+
+//Вывод модального окна командой alert
 alert ("Hello Glo Academy!");
+
+//Вывод лога в коноль командой console.log
+console.log("title ",title ,"screens ", screens,"screenprice ", screenprice, "rollback ", rollback, "fullprice ", fullprice,"adaptive ", adaptive);
